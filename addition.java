3e0f0dba6dcfd6209266b614cf494a1fd1df822b
Kiran Fartade <kiran.fartade@mindstix.com>
2023-02-04 13:16:48 +0530

    updated addition file with added k value to 5

--- addition.java
+++ addition.java
@@ -1,5 +1,6 @@
 class add{
     public static void main(String[] args) {
-        System.out.println("addition class updated ");
+        int k = 1;
+        System.out.println("addition class updated, added k value to 5 ");
     }
 }
