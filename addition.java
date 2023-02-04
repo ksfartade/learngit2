473aac32a171cf9526b25b75d2db93ece018bd15
kiran fartade <75989843+ksfartade@users.noreply.github.com>
2023-02-04 13:18:56 +0530

    Update addition.java
    properly updated k value to 5

--- addition.java
+++ addition.java
@@ -1,6 +1,6 @@
 class add{
     public static void main(String[] args) {
-        int k = 1;
+        int k = 5;
         System.out.println("addition class updated, added k value to 5 ");
     }
 }
